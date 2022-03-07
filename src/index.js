ee6d180e307665746cd53a36a22f4018bf075f6a
Barysenka <iborisenkoip@gmail.com>
2022-03-07 12:50:38 +0100

    feat: add function towelSoft

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let newArr = []
+   if (!matrix){
+    return newArr
+   } else{
+  matrix.forEach((elem,ind) => {
+  
+        if(ind % 2 === 0){   
+            newArr = newArr.concat(elem)
+
+        }else{
+            elem.reverse()
+            newArr = newArr.concat(elem)
+        }
+        
+    });
+  }
+    return newArr
 }
